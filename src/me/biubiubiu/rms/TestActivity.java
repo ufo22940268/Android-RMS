731584b61fb8c42ce2e43f81ece5d9c1eca1aeed
garlic <ufo22940268@gmail.com>
2013-11-20 18:36:28 +0800

    asdf

---
+++ src/me/biubiubiu/rms/TestActivity.java
@@ -0,0 +1,49 @@
+/*
+ * BaseActivity.java
+ * Copyright (C) 2013 garlic <garlic@meishixing>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+package me.biubiubiu.rms;
+
+import android.util.*;
+import android.widget.*;
+import android.view.*;
+import android.content.*;
+import android.content.res.*;
+import android.app.*;
+import android.os.*;
+import android.database.*;
+import android.net.*;
+import android.opengl.*;
+import android.graphics.*;
+import android.view.animation.*;
+
+import java.util.*;
+import org.json.*;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import com.viewpagerindicator.PageIndicator;
+import com.viewpagerindicator.TabPageIndicator;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import com.andreabaccega.widget.FormEditText;
+
+import java.util.*;
+import java.lang.reflect.*;
+import org.json.*;
+import me.biubiubiu.rms.util.HttpHandler.ResponseHandler;
+import me.biubiubiu.rms.util.*;
+import me.biubiubiu.rms.ui.*;
+import me.biubiubiu.rms.model.*;
+import me.biubiubiu.rms.*;
+import com.loopj.android.http.*;
+import com.kanak.emptylayout.EmptyLayout;
+
+public class TestActivity extends ActionBarActivity {
+}
